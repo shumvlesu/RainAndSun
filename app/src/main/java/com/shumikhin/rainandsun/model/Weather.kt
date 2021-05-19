18921ce7f0ae242002cd8063bbd31844fb34e89f
sergey <sashumihin@gmail.com>
2021-05-19 16:48:13 +0300

    Дизайн!

--- app/src/main/java/com/shumikhin/rainandsun/model/Weather.kt
+++ app/src/main/java/com/shumikhin/rainandsun/model/Weather.kt
@@ -2,8 +2,8 @@ package com.shumikhin.rainandsun.model
 
 data class Weather(
     val city: City = getDefaultCity(),
-    val temperature: Int = 0,
-    val feelsLike: Int = 0
+    val temperature: Int = 15,
+    val feelsLike: Int = 13
 )
 
 fun getDefaultCity() = City("Москва", 55.755826, 37.617299900000035)
