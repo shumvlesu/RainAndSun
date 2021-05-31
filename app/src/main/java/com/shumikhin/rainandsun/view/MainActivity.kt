341321402a207ed0bc7dac318b30f3eb4406584f
sergey <sashumihin@gmail.com>
2021-05-31 13:06:40 +0300

    Простой браузер.

--- app/src/main/java/com/shumikhin/rainandsun/view/MainActivity.kt
+++ app/src/main/java/com/shumikhin/rainandsun/view/MainActivity.kt
@@ -1,27 +1,74 @@
 package com.shumikhin.rainandsun.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import com.shumikhin.rainandsun.databinding.MainActivityBinding
-import com.shumikhin.rainandsun.view.details.DetailsFragment
-import com.shumikhin.rainandsun.view.main.MainFragment
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.shumikhin.rainandsun.databinding.MainActivityWebviewBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
     //Использую ViewBinding
-    private lateinit var binding: MainActivityBinding
+    private lateinit var binding: MainActivityWebviewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = MainActivityBinding.inflate(layoutInflater)
+        binding = MainActivityWebviewBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                //.replace(R.id.container, MainFragment.newInstance())
-                .replace(binding.container.id, MainFragment.newInstance())
-                .commitNow()
+        binding.ok.setOnClickListener(clickListener)
+    }
+
+    private var clickListener: View.OnClickListener = object : View.OnClickListener {
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun onClick(v: View?) {
+            try {
+                val uri = URL(binding.url.text.toString())
+                val handler = Handler() //Запоминаем основной поток
+                Thread {
+                    var urlConnection: HttpsURLConnection? = null
+                    try {
+                        urlConnection = uri.openConnection() as HttpsURLConnection
+                        urlConnection.requestMethod = "GET" //установка метода получения данных — GET
+                        urlConnection.readTimeout = 10000 //установка таймаута — 10 000 миллисекунд
+                        val reader =
+                            BufferedReader(InputStreamReader(urlConnection.inputStream)) //читаем данные в поток
+                        val result = getLines(reader)
+
+                        // Возвращаемся к основному потоку
+                        handler.post {
+                            binding.webview.loadData(result, "text/html; charset=utf-8", "utf-8")
+                        }
+                    } catch (e: Exception) {
+                        Log.e("", "Fail connection", e)
+                        e.printStackTrace()
+                    } finally {
+                        urlConnection?.disconnect()
+                    }
+                }.start()
+            } catch (e: MalformedURLException) {
+                Log.e("", "Fail URI", e)
+                e.printStackTrace()
+            }
+        }
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        private fun getLines(reader: BufferedReader): String {
+            return reader.lines().collect(Collectors.joining("\n"))
         }
     }
+
 }
+
+
 
