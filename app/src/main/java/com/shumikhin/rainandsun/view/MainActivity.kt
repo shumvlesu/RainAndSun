b36b650e55a64975d58e3d992fd95de29afce445
sergey <sashumihin@gmail.com>
2021-06-12 16:38:00 +0300

    ДЗ 8 урок. Готово.

--- app/src/main/java/com/shumikhin/rainandsun/view/MainActivity.kt
+++ app/src/main/java/com/shumikhin/rainandsun/view/MainActivity.kt
@@ -3,9 +3,13 @@ package com.shumikhin.rainandsun.view
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.shumikhin.rainandsun.R
 import com.shumikhin.rainandsun.databinding.MainActivityBinding
 import com.shumikhin.rainandsun.view.details.ConnectivityBroadcastReceiverPrimer
+import com.shumikhin.rainandsun.view.history.HistoryFragment
 import com.shumikhin.rainandsun.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -32,5 +36,27 @@ class MainActivity : AppCompatActivity() {
         unregisterReceiver(receiver)
         super.onDestroy()
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_screen_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_history -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .add(R.id.container, HistoryFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
 
